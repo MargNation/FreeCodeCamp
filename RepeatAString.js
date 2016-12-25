e231a7d9ea4245a1859b3f29ffa62e5a86fc3294
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-12-25 15:36:14 -0500

    Create RepeatAString.js

---
+++ RepeatAString.js
@@ -0,0 +1,18 @@
+//Repeat a given string (first argument) num times (second argument). 
+//Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  toReturn = "";
+  for (var i = 0; i < num; i++) {
+    toReturn += str;
+  }
+  return toReturn;
+}
+
+repeatStringNumTimes("*", 3) //should return "***".
+repeatStringNumTimes("abc", 3) //should return "abcabcabc".
+repeatStringNumTimes("abc", 4) //should return "abcabcabcabc".
+repeatStringNumTimes("abc", 1) //should return "abc".
+repeatStringNumTimes("*", 8) //should return "********".
+repeatStringNumTimes("abc", -2) //should return ""
