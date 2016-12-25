6ba6d6987dcd82a532a5caf791ef765e1e6005ba
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-12-25 15:46:38 -0500

    Update RepeatAString.js

--- RepeatAString.js
+++ RepeatAString.js
@@ -10,9 +10,9 @@ function repeatStringNumTimes(str, num) {
   return toReturn;
 }
 
-repeatStringNumTimes("*", 3) //should return "***".
-repeatStringNumTimes("abc", 3) //should return "abcabcabc".
-repeatStringNumTimes("abc", 4) //should return "abcabcabcabc".
-repeatStringNumTimes("abc", 1) //should return "abc".
-repeatStringNumTimes("*", 8) //should return "********".
-repeatStringNumTimes("abc", -2) //should return ""
+repeatStringNumTimes("*", 3); //should return "***".
+repeatStringNumTimes("abc", 3); //should return "abcabcabc".
+repeatStringNumTimes("abc", 4); //should return "abcabcabcabc".
+repeatStringNumTimes("abc", 1); //should return "abc".
+repeatStringNumTimes("*", 8); //should return "********".
+repeatStringNumTimes("abc", -2); //should return ""
